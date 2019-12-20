905a62f3b916f5f804780b87ff97a81791e034b4
Seyyed Parsa Neshaei <spneshaei@gmail.com>
2019-12-20 13:45:04 +0330

    Initial Commit

---
+++ CGramServer/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  CGramServer
+//
+//  Created by Seyyed Parsa Neshaei on 12/20/19.
+//  Copyright © 2019 Seyyed Parsa Neshaei. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
